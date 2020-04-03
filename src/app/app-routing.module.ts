1d2c55cc6cd7480cc751e9d7d395835f8edf0b46
Adam <balassaadi@gmail.com>
2020-04-03 23:51:03 +0200

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './main/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent, pathMatch: 'prefix', children: [
+    { path: '', component: IndexComponent, pathMatch: 'full' }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { IndexComponent } from './main/index/index.component';
+import { HeaderComponent } from './main/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

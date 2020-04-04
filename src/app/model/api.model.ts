a165e773b26e05a8d2efdba1fbd96721ec37d05e
DantElemer <farkasdomonkosl@gmail.com>
2020-04-04 14:45:36 +0200

    towns, offers

--- src/app/model/api.model.ts
+++ src/app/model/api.model.ts
@@ -25,3 +25,40 @@ export interface NeedsUpdate {
     flatSpec?: any;
     paymentMethod?: string;
 }
+
+export interface Town {
+    town_id: number;
+    ksh_id: number;
+    zipcode: number;
+    country_id: number;
+    country_name: string;
+    town_name: string;
+}
+
+export interface Towns {
+    items: Town[];
+}
+
+export interace Offers {
+    type: string;
+    description: string;
+    location: any;
+    level: string;
+    flatSpec: any;
+    paymentMethod: string;
+    hasCar: boolean;
+    experience: string;
+    skills: string[];
+}
+
+export interace OffersUpdate {
+    type?: string;
+    description?: string;
+    location?: any;
+    level?: string;
+    flatSpec?: any;
+    paymentMethod?: string;
+    hasCar?: boolean;
+    experience?: string;
+    skills?: string[];
+}
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { SomeResult, Needs, NeedsUpdate } from '../model/api.model';
+import { SomeResult, Needs, NeedsUpdate, Towns, Offers, OffersUpdate } from '../model/api.model';
 import { Request } from '../model/request.model';
 
 @Injectable({
@@ -9,6 +9,18 @@ import { Request } from '../model/request.model';
 export class ServerService {
 
   constructor(private http: HttpClient) { }
+  
+  
+  
+  getTowns(): Promise<Towns> {
+    return new Request<Towns>(this.http).get('/towns');
+  }
+  
+  getTownsByZipcode(zipcode: string): Promise<Towns> {
+    return new Request<Towns>(this.http).get(`/towns/${zipcode}`);
+  }
+  
+  
   
   postNeeds(needs: Needs): Promise<SomeResult> {
     return new Request<SomeResult>(this.http).post('/needs', needs)
@@ -24,6 +36,24 @@ export class ServerService {
   
   patchNeedsById(id: string, needs: NeedsUpdate): Promise<SomeResult> {
     return new Request<NeedsUpdate>(this.http).patch(`/needs/${id}`, needs);
+  }
+  
+  
+  
+  postOffers(offers: Offers): Promise<SomeResult> {
+    return new Request<SomeResult>(this.http).post('/offers', offers)
+  }
+  
+  getOffers(): Promise<Offers> {
+    return new Request<Offers>(this.http).get('/offers');
+  }
+  
+  getOffersById(offerId: string): Promise<Offers> {
+    return new Request<Offers>(this.http).get(`/offers/${offerId}`);
+  }
+  
+  patchOffersById(offerId: string, offers: OffersUpdate): Promise<SomeResult> {
+    return new Request<OffersUpdate>(this.http).patch(`/offers/${offerId}`, offers);
   }
   
 }
